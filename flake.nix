e52e09ac399dcc339af25b66f47f3caa902d56da
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 15:41:05 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
-  };
-  outputs = { self, nixpkgs }@attrs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
-      pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; };};
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix
-                  # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
-                  ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
-                ];
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-25.11";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            input.nixplgs.follows = "nixpkgs";
+        };
     };
-  };
+    outputs = { self, nixpkgs home-manager }@attrs: {
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
+            pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; };};
+            system = "x86_64-linux";
+            modules = [ 
+                ./configuration.nix
+                ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
+            ];
+        };
+    };
 }
