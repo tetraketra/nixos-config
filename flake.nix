815fee62e97608852c5df20bd4c061d608e94ac8
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 15:22:43 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "nixpkgs/nixos-25.11";
   };
   outputs = { self, nixpkgs }@attrs: {
-    nixosConfigurations.motoko = nixpkgs.lib.nixosSystem rec {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
       pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; };};
       system = "x86_64-linux";
       modules = [ ./configuration.nix
