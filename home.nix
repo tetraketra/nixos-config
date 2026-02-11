caba8228117f9f7169e7fc47f1bb9db40f81ba13
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 14:52:21 -0500

    Working on home-manager.

--- home.nix
+++ home.nix
@@ -1,14 +1,24 @@
 { pkgs, ... }:
 {
-    gtk = {
-        enable = true;
-        theme = {
-            name = "Ad-dark";
-            package = pkgs.materia-theme;
-        };
-        iconTheme = {
-            name = "Papirus-Dark";
-            package = pkgs.papirus-icon-theme;
-        };
+    # gtk = {
+    #     enable = true;
+    #     theme = {
+    #         name = "Ad-dark";
+    #         package = pkgs.materia-theme;
+    #     };
+    #     iconTheme = {
+    #         name = "Papirus-Dark";
+    #         package = pkgs.papirus-icon-theme;
+    #     };
+    # };
+    home = {
+        packages = with pkgs; [
+            hello
+        ];
+
+        username = "tetraketra";
+        homeDirectory = "/home/tetraketra";
+
+        stateVersion = "25.11";
     };
 }
--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,7 @@ set -e
 REPO_HOME="$(git rev-parse --show-toplevel)"
 
 # Cleanup.
+echo "🗑️ Defining cleanup on-exit step."
 cleanup() {
     if [ -L "$REPO_HOME/hardware-configuration.nix" ]; then
         unlink "$REPO_HOME/hardware-configuration.nix"
@@ -11,18 +12,25 @@ cleanup() {
 trap cleanup EXIT
 
 # Link repo to `/etc/nixos`.
+echo "🔗 Linking repo to \`/etc/nixos\`."
 sudo mv /etc/nixos /etc/nixos.bak
 sudo ln -sf $REPO_HOME/hosts/$1/hardware-configuration.nix $REPO_HOME/hardware-configuration.nix
 sudo ln -s $REPO_HOME /etc/nixos
 
 # Move dotfiles.
+echo "🔗 Linking dotfiles to \`~\`."
 sudo ln -sf $REPO_HOME/dotfiles/.zshrc ~/.zshrc
 
 # Rebuild.
+echo "🏗️ Rebuilding."
 sudo nixos-rebuild switch
 
 # Cinnamon.
+echo "🫚 Updating cinnamon."
 gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark-Teal"
 gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y-Dark-Teal"
 gsettings set org.cinnamon.desktop.interface icon-theme "Mint-L-Teal"
 cinnamon --replace &
+
+# Final.
+echo "✅ Complete!"
