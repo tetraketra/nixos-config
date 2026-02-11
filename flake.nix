a3537ed0993525d43c6ae4019a23eecaf49ee001
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 15:18:55 -0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+  };
+  outputs = { self, nixpkgs }@attrs: {
+    nixosConfigurations.motoko = nixpkgs.lib.nixosSystem rec {
+      pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; };};
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix
+                  # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
+                  ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
+                ];
+    };
+  };
+}
