d1f67561074ff8c0b47a8f2ed7acec12f2474cf3
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 13:44:09 -0500

    Splitting oauth redirect into a new nix file.

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./oauth-redirect.nix
   ];
 
   # Foundation
@@ -19,7 +20,7 @@
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_USd.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
     LC_MONETARY = "en_US.UTF-8";
     LC_NAME = "en_US.UTF-8";
     LC_NUMERIC = "en_US.UTF-8";
@@ -48,34 +49,6 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  # Enable oauth redirections.
-  xdg = {
-    autostart.enable = true;
-    portal = {
-      xdgOpenUsePortal = true;
-      enable = true;
-      extraPortals = [
-        pkgs.xdg-desktop-portal
-        pkgs.xdg-desktop-portal-gtk
-        pkgs.xdg-desktop-portal-xapp
-      ];
-    };
-  }; 
-
-  environment.sessionVariables = {
-    XDG_SESSION_TYPE = "x11";
-    XDG_CURRENT_DESKTOP = "X-Cinnamon";
-    XDG_SESSION_DESKTOP = "X-Cinnamon";
-    GTK_USE_PORTAL = "1";
-  };
-
-  environment.systemPackages = with pkgs; [
-    xdg-utils
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-xapp
-  ];
 
   # Define user account.
   users.users.tetraketra = {
---
+++ oauth-redirect.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  xdg = {
+    autostart.enable = true;
+    portal = {
+      xdgOpenUsePortal = true;
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-xapp
+      ];
+    };
+  }; 
+
+  environment.sessionVariables = {
+    XDG_SESSION_TYPE = "x11";
+    XDG_CURRENT_DESKTOP = "X-Cinnamon";
+    XDG_SESSION_DESKTOP = "X-Cinnamon";
+    GTK_USE_PORTAL = "1";
+  };
+
+  environment.systemPackages = with pkgs; [
+    xdg-utils
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-xapp
+  ];
+}
+  
