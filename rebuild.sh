42f4355fabbf96f99e5ba462f045245a87b3c7e9
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 13:22:20 -0500

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,14 @@
+# Setup
+set -e
 REPO_HOME="$(git rev-parse --show-toplevel)"
 
+# Link repo to `/etc/nixos`.
 sudo mv /etc/nixos /etc/nixos.bak
 sudo ln -sf $REPO_HOME/hosts/$1/hardware-configuration.nix $REPO_HOME/hardware-configuration.nix
 sudo ln -s $REPO_HOME /etc/nixos
 
+# Rebuild.
 sudo nixos-rebuild switch
+
+# Cleanup
+unlink $REPO_HOME/hardware-configuration.nix
