8be51a88048e228516de4aa2e777282047876982
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 15:34:20 -0500

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,11 +2,21 @@
 set -e
 REPO_HOME="$(git rev-parse --show-toplevel)"
 
+# Cleanup.
+echo "🗑️  Defining cleanup on-exit step."
+cleanup() {
+    if [ -L "$REPO_HOME/hardware-configuration.nix" ]; then
+        unlink "$REPO_HOME/hardware-configuration.nix"
+    fi
+}
+trap cleanup EXIT
+
 # Link repo to `/etc/nixos`.
 echo "🔗 Linking repo to \`/etc/nixos\`."
 sudo mv /etc/nixos /etc/nixos.bak
+sudo ln -sf $REPO_HOME/hosts/$1/hardware-configuration.nix $REPO_HOME/hardware-configuration.nix
+git add $REPO_HOME/hardware-configuration.nix
 sudo ln -s $REPO_HOME /etc/nixos
-sudo ln -s $REPO_HOME/hosts/$1/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
 # Move dotfiles.
 echo "🔗 Linking dotfiles to \`~\`."
