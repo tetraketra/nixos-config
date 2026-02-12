46b39b5a68e7a39e564a903e2d2b381d113b8223
Tetra Silverberg <tetra.silv@gmail.com>
2026-02-11 19:59:28 -0500

    Gamedev.

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
         ./oauth-redirect.nix
         ./cinnamon.nix
         ./default-apps.nix
-        ./rust.nix
+        ./development.nix
     ];
 
     # Foundation
--- development.nix
+++ development.nix
@@ -2,11 +2,23 @@
 
 {
     environment.systemPackages = with pkgs; [
+        # C.
+        nix-ld
+        libgcc
+        # Rust.
         cargo
         rustc
         rustfmt
         clippy
         rust-analyzer
+        # Python.
+        python312
+        uv
+        # Games.
+        sdl3
+        SDL2
+        glfw
+        raylib
     ];
 
     environment.sessionVariables = {
