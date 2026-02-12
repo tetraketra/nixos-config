ab58ec846c7e32f4870d2178aca90e57d014a227
Tetra <tetra.silv@gmail.com>
2026-02-12 01:56:02 -0500

    .

--- homes/tetraketra.nix
+++ homes/tetraketra.nix
@@ -1,10 +1,34 @@
 { inputs, lib, pkgs, ... }:
+
+let
+  keybinds-generator = bindlist:
+    let
+      custom-names = map (i: "custom${toString i}") (pkgs.lib.lists.range 0 (builtins.length bindlist - 1));
+      zipped = pkgs.lib.lists.zipListsWith (n: b: { name = n; bind = b; }) custom-names bindlist;
+    in {
+            "org/cinnamon/desktop/keybindings" = { custom-list = custom-names; };
+      } // builtins.listToAttrs (
+        map (item: {
+            name = "org/cinnamon/desktop/keybindings/custom-keybindings/${item.name}";
+            value = {
+                binding = item.bind.binding;
+                command = item.bind.command;
+                name = item.bind.name;
+            };
+        }) zipped
+    );
+in
 {
     home = {
         packages = with pkgs; [
             hello
         ];
-
-        # and so on
     };
+
+    programs.dconf.enable = true;
+    programs.dconf.settings = keybinds-generator [
+        { binding = ["<Primary><Alt>f"]; command = "firefox"; name = "Launch Firefox"; }
+        { binding = ["<Primary><Alt>c"]; command = "qalculate-gtk"; name = "Launch Qalculate-GTK (C)"; }
+        { binding = ["<Primary><Alt>q"]; command = "qalculate-gtk"; name = "Launch Qalculate-GTK (Q)"; }
+    ];
 }
--- nixos/modules/default-apps.nix
+++ nixos/modules/default-apps.nix
@@ -1,31 +1,8 @@
 { config, pkgs-stable, ... }:
-
+# TODO: TO BE CONVERTED TO HOME-MANAGER ACTUAL USER PROFILE (*NOT JUST DEFAULTS*)
 let
     image-types = [ "png" "jpeg" "jpg" "gif" "svg+xml" "bmp" "webp" "tiff" "x-icon" "vnd.microsoft.icon" "heif" "heic" "avif" "x-pcx" "x-pnm" "x-portable-bitmap" "x-portable-graymap" "x-portable-pixmap" "x-xbitmap" "x-xpixmap" ];
     image-formats = map (type: "image/${type}") image-types;
-
-    keybindings = [
-        { binding=["<Primary><Alt>t"]; command="alacritty -e"; name="Launch Alacritty"; }
-        { binding=["<Primary><Alt>f"]; command="firefox"; name="Launch Firefox"; }
-        { binding=["<Primary><Alt>c"]; command="qalculate-gtk"; name="Launch Qalculate-GTK (C)"; }
-        { binding=["<Primary><Alt>q"]; command="qalculate-gtk"; name="Launch Qalculate-GTK (Q)"; }
-    ];
-
-    custom-names = map (i: "custom${toString i}") (builtins.range 0 (builtins.length keybindings - 1));
-    zipped = pkgs-stable.lib.lists.zipListsWith (name: bind: { name=name; bind=bind; }) custom-names keybindings;
-
-    generated-keybinds = {
-        "org/cinnamon/desktop/keybindings" = { custom-list = custom-names; };
-    } // builtins.listToAttrs (
-        map (item: {
-            name = "org/cinnamon/desktop/keybindings/custom-keybindings/${item.name}";
-            value = {
-                binding = item.bind.binding;
-                command = item.bind.command;
-                name = item.bind.name;
-            };
-        }) zipped
-    );
 in
 {
     environment.systemPackages = with pkgs-stable; [
@@ -37,7 +14,6 @@ in
     ];
 
     programs.dconf.profiles.user.databases = [{
-        # TODO: TO BE CONVERTED TO HOME-MANAGER ACTUAL USER PROFILE (*NOT JUST DEFAULTS*)
         settings = {
             "org/cinnamon/desktop/applications/terminal" = {
                 exec = "alacritty";
@@ -47,7 +23,7 @@ in
             "org/cinnamon/desktop/applications/calculator" = {
                 exec = "qalculate-gtk";
             };            
-        } // generated-keybinds;
+        };
     }];
 
     xdg.mime.defaultApplications = builtins.listToAttrs (
