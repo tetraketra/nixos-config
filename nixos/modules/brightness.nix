aa2a3f384753252fd25bb256df37d4eb716b5295
Tetra <tetra.silv@gmail.com>
2026-04-03 15:44:26 -0400

    .

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -5,27 +5,22 @@ let
     should-bright = builtins.elem (builtins.getEnv "TARGET") enable-on-targets;
 in
 {
-    systemd.timers."set-gamma" = lib.mkIf should-bright {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
+    systemd.user.services."set-gamma" = lib.mkIf should-bright {
+        Unit.After = [ "graphical-session.target" ];
+        Install.WantedBy = [ "graphical-session.target" ];
+
+        Service = {
+            Type = "oneshot";
+            ExecStart = "${pkgs-stable.xorg.xrandr}/bin/xrandr --output eDP-1 --brightness 2";
+        };        
+    };
+
+    systemd.user.timers."set-gamma" = lib.mkIf should-bright {
+        Install.WantedBy = [ "timers.target" ];
+
+        Timer = {
             OnBootSec = "2m";
             OnUnitActiveSec = "2m";
-            Unit = "set-gamma.service";
         };
     };
-
-    systemd.services."set-gamma" = lib.mkIf should-bright {
-        wantedBy = [ "graphical.target" ];
-        after = [ "graphical.target" ];
-        restartIfChanged = true;
-        script = ''
-            ${pkgs-stable.xorg.xrandr}/bin/xrandr --output eDP-1 --brightness 2
-        '';
-        serviceConfig = {
-            Type = "oneshot";
-            User = "tetraketra";
-            RemainAfterExit = true;
-        };
-    };
-
 }
