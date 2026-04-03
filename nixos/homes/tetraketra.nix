5c80a3c64ce775daeef2fd3a9339c16be06f8d3c
Tetra <tetra.silv@gmail.com>
2026-04-03 16:30:42 -0400

    .

--- nixos/homes/tetraketra.nix
+++ nixos/homes/tetraketra.nix
@@ -1,25 +1,6 @@
 { config, lib, inputs, pkgs-unstable, pkgs-stable, ... }:
 
 let
-    # Set Desktop Keybinds
-    keybinds-generator = bindlist:
-        let
-            custom-names = map (i: "custom${toString i}") (pkgs-stable.lib.lists.range 0 (builtins.length bindlist - 1));
-            zipped = pkgs-stable.lib.lists.zipListsWith (n: b: { name = n; bind = b; }) custom-names bindlist;
-        in {
-            "org/cinnamon/desktop/keybindings" = { custom-list = custom-names; };
-        } // builtins.listToAttrs (
-            map (item: {
-                name = "org/cinnamon/desktop/keybindings/custom-keybindings/${item.name}";
-                value = {
-                    binding = item.bind.binding;
-                    command = item.bind.command;
-                    name = item.bind.name;
-                };
-            }) zipped
-        );
-
-
     # Fix Brightness
     enable-on-targets = [ "hp-envy" ];
     should-bright = builtins.elem (builtins.getEnv "TARGET") enable-on-targets;
@@ -27,23 +8,13 @@ let
 
     files = [
         ../modules/pinta.nix
+        ../modules/keybinds.nix
     ];
 
     configs = map (f: import f { inherit config lib inputs pkgs-stable pkgs-unstable; }) files;
     merged = lib.foldl (a: b: a // b) {} configs;
 in
 merged // {
-    # Set Desktop Keybinds
-    dconf.enable = true;
-    dconf.settings = keybinds-generator [
-        { binding = ["<Primary><Alt>f"]; command = "firefox"; name = "Launch Firefox (F)"; }
-        { binding = ["<Primary><Alt>c"]; command = "qalculate-gtk -n"; name = "Launch Qalculate-GTK (C)"; }
-        { binding = ["<Primary><Alt>q"]; command = "qalculate-gtk -n"; name = "Launch Qalculate-GTK (Q)"; }
-        { binding = ["<Primary><Alt>b"]; command = "alacritty -e btop"; name = "Launch BTop (B)"; }
-        { binding = ["<Primary><Alt>p"]; command = "pinta"; name = "Launch Pinta (P)"; }
-        { binding = ["<Primary><Shift>s"]; command = "shutter -s"; name = "Launch Shutter (S)"; }
-    ];
-
     # Link Dotfiles
     home.file.".zshrc".source = ../../dotfiles/.zshrc;
     home.file.".bashrc".source = ../../dotfiles/.bashrc;
---
+++ nixos/modules/keybinds.nix
@@ -0,0 +1,31 @@
+{ config, lib, inputs, pkgs-unstable, pkgs-stable, ... }:
+
+let
+    keybinds-generator = bindlist:
+        let
+            custom-names = map (i: "custom${toString i}") (pkgs-stable.lib.lists.range 0 (builtins.length bindlist - 1));
+            zipped = pkgs-stable.lib.lists.zipListsWith (n: b: { name = n; bind = b; }) custom-names bindlist;
+        in {
+            "org/cinnamon/desktop/keybindings" = { custom-list = custom-names; };
+        } // builtins.listToAttrs (
+            map (item: {
+                name = "org/cinnamon/desktop/keybindings/custom-keybindings/${item.name}";
+                value = {
+                    binding = item.bind.binding;
+                    command = item.bind.command;
+                    name = item.bind.name;
+                };
+            }) zipped
+        );
+in
+{
+    dconf.enable = true;
+    dconf.settings = keybinds-generator [
+        { binding = ["<Primary><Alt>f"]; command = "firefox"; name = "Launch Firefox (F)"; }
+        { binding = ["<Primary><Alt>c"]; command = "qalculate-gtk -n"; name = "Launch Qalculate-GTK (C)"; }
+        { binding = ["<Primary><Alt>q"]; command = "qalculate-gtk -n"; name = "Launch Qalculate-GTK (Q)"; }
+        { binding = ["<Primary><Alt>b"]; command = "alacritty -e btop"; name = "Launch BTop (B)"; }
+        { binding = ["<Primary><Alt>p"]; command = "pinta"; name = "Launch Pinta (P)"; }
+        { binding = ["<Primary><Shift>s"]; command = "shutter -s"; name = "Launch Shutter (S)"; }
+    ];
+}
