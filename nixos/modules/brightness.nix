46b110690704099c86dadede9e0af8457f4f1bf8
Tetra <tetra.silv@gmail.com>
2026-04-03 15:52:19 -0400

    .

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -5,23 +5,21 @@ let
     should-bright = builtins.elem (builtins.getEnv "TARGET") enable-on-targets;
 in
 {
-    systemd.user.services."set-gamma" = lib.mkIf should-bright {
-        description = "Boost brightness (gamma) on dim targets.";
-        wantedBy = [ "graphical.target" ]; 
-        after = [ "graphical.target" ];
-        restartIfChanged = true;
-        serviceConfig = {
-            Type = "oneshot";
-            ExecStart = "${pkgs-stable.xorg.xrandr}/bin/xrandr --output eDP-1 --brightness 2";
-        };
+    systemd.services.set-gamma = {
+        description = "My periodic task service";
+        serviceConfig.Type = "oneshot";
+        script = ''
+            ${pkgs-stable.xorg.xrandr}/bin/xrandr --output eDP-1 --brightness 2
+        '';
     };
 
-    systemd.user.timers."set-gamma" = lib.mkIf should-bright {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-            OnBootSec = "2m";
-            OnUnitActiveSec = "2m";
-            Unit = "set-gamma.service";
-        };
+    systemd.timers.set-gamma = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+        OnBootSec = "15s";
+        OnUnitActiveSec = "60s";
+        Unit = "set-gamma.service";
     };
+    };
+
 }
