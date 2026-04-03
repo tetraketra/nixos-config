a1eee6b6735461f12ce87f065511dbb230a0b4fc
Tetra <tetra.silv@gmail.com>
2026-04-03 15:48:48 -0400

    .

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -6,24 +6,22 @@ let
 in
 {
     systemd.user.services."set-gamma" = lib.mkIf should-bright {
-        description = "Set screen brightness via xrandr";
-
-        wantedBy = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-
+        description = "Boost brightness (gamma) on dim targets.";
+        wantedBy = [ "graphical.target" ]; 
+        after = [ "graphical.target" ];
+        restartIfChanged = true;
         serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs-stable.xorg.xrandr}/bin/xrandr --output eDP-1 --brightness 2";
+            Type = "oneshot";
+            ExecStart = "${pkgs-stable.xorg.xrandr}/bin/xrandr --output eDP-1 --brightness 2";
         };
     };
 
     systemd.user.timers."set-gamma" = lib.mkIf should-bright {
         wantedBy = [ "timers.target" ];
-
         timerConfig = {
-        OnBootSec = "2m";
-        OnUnitActiveSec = "2m";
-        Unit = "set-gamma.service";
+            OnBootSec = "2m";
+            OnUnitActiveSec = "2m";
+            Unit = "set-gamma.service";
         };
     };
 }
