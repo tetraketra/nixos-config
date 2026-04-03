f4e6109253390109e98653123f927e393e825c73
Tetra <tetra.silv@gmail.com>
2026-04-03 14:58:59 -0400

    .

--- homes/tetraketra.nix
+++ homes/tetraketra.nix
@@ -40,7 +40,7 @@ in
     home.file.".zshrc".source = ../dotfiles/.zshrc;
     home.file.".bashrc".source = ../dotfiles/.bashrc;
     home.file.".alacritty.toml".source = ../dotfiles/.alacritty.toml;
-    xdg.configFile."nemo".source = ./dotfiles/nemo;
+    xdg.configFile."nemo".source = ../dotfiles/nemo;
 
     # ln -sf $REPO_HOME/dotfiles/nemo-desktop-metadata $USER_HOME/.config/nemo/desktop-metadata
 }
