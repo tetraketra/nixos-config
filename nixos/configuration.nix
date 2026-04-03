2baf3dd18f517299f0769fc34f493eba17804834
Tetra <tetra.silv@gmail.com>
2026-04-03 14:06:54 -0400

    Adding new host.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,9 +11,11 @@
     ];
 
     # Foundation
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
-    boot.loader.grub.useOSProber = true;
+    # boot.loader.grub.enable = true;
+    # boot.loader.grub.device = "/dev/sda";
+    # boot.loader.grub.useOSProber = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
     networking.hostName = "nixos";
     networking.networkmanager.enable = true;
